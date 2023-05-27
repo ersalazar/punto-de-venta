e2974dc0e89be45b290b856a35749c7ab7c9be9d
ersalazar <71133367+ersalazar@users.noreply.github.com>
2023-05-26 18:25:13 -0600

    added button for new sale

--- src/screens/Sales.tsx
+++ src/screens/Sales.tsx
@@ -1,9 +1,15 @@
 //@ts-nocheck
+import { Button } from '@mui/material';
+import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation';
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { getProduct } from '../firebase/ProductsFirebase.tsx';
 import { getSales } from '../firebase/SalesFirebase.tsx';
+import {getService} from '../firebase/ServicesFirebase.tsx';
 
 function Sales() {
   const [sales, setSales] = useState([]);
+  const [names, setNames] = useState<string[]>([]);
 
   useEffect(() => {
     fetchSalesData();
@@ -23,6 +29,46 @@ function Sales() {
     return date.toLocaleString(); // Adjust the formatting as per your preference
   };
 
+  useEffect(() => {
+    const getAndSetNames = async() => {
+
+      const names = await getNames(sales)
+      setNames(names)
+  }
+  getAndSetNames()
+
+  }, [sales])
+
+  const getNames = async (saleData) => {
+    try {
+      const names  : string[] = []
+      saleData.forEach(sale => {
+        const refs = sale.items;
+        refs.forEach ( async ref => {
+          const product = ref.split('/')
+          const type = product[0]
+          const id  = product[1]
+          console.log('ID', id)
+          console.log('TYPE', typeImplementation)
+          let name
+          if (type ==='products'){
+              name = (await getProduct(id)).data().name
+          }
+          else {
+            name = (await getService(id)).data().name
+          }
+
+          if(name) {
+            names.push(name)
+          }
+        });
+        console.log('NAMES',names)
+      }) 
+    } catch (error) {
+      console.error('Error fetching sales data:', error);
+    }
+  };
+  
   return (
     <div className="container mt-4">
       <h1>Sales</h1>
@@ -40,7 +86,7 @@ function Sales() {
           {sales.map((sale) => {
             const id = sale.id;
             const { items, quantities, date, total } = sale.data();
-
+            console.log('names', names)
             return (
               <tr key={id}>
                 <td>{id}</td>
@@ -52,7 +98,8 @@ function Sales() {
             );
           })}
         </tbody>
-      </table>
+      </table> 
+      <NavLink classNmae='btn btn-info mx-2' to={'/newSale'}>Agregar venta</NavLink>
     </div>
   );
 }
